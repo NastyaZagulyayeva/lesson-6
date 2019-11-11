dab32dd21914f35ca8707e0d8008ce82697b6c2e
Sandrik85 <sandrik85@gmail.com>
2019-11-11 23:13:49 +0200

    Comment about your work

--- js/custom.js
+++ js/custom.js
@@ -8,13 +8,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-var myNav = document.getElementById('nav-bar');
-window.onscroll = function () { 
+var myNav = document.getElementById('nav-bar'); /*Вместо var в современном JS пишут let*/
+window.onscroll = function () {
     "use strict";
     if (document.scrollingElement.scrollTop >= 759 ) {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
-    } 
+    }
     else {
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
